9111d5ee612cb608d1abdf85e1407f8fa14a5df5
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-27 23:45:05 -0700

    annotation for JPA

--- src/main/java/io/aharoj/jobapplication/job/Job.java
+++ src/main/java/io/aharoj/jobapplication/job/Job.java
@@ -1,5 +1,10 @@
 package io.aharoj.jobapplication.job;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,12 +18,15 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+// @Table(name="job_table")
 public class Job {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String title;
   private String description;
   private String minSalary;
   private String maxSalary;
   private String location;
-
 }
