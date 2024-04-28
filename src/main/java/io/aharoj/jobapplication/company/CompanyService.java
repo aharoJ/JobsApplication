3230b52875c4752c6182e153bfb4f5c5f621c6e5
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-28 16:22:58 -0700

    company interface

---
+++ src/main/java/io/aharoj/jobapplication/company/CompanyService.java
@@ -0,0 +1,11 @@
+package io.aharoj.jobapplication.company;
+
+import java.util.List;
+
+public interface CompanyService {
+  List<Company> getAllCompanies();
+  boolean updateCompany(Long id, Company company);
+  void createCompany(Company company);
+
+
+}
