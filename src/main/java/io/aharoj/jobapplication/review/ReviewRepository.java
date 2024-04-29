22318f2b7fe8d2633a564225d7fe025da423bc45
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-29 15:44:55 -0700

    review repository

---
+++ src/main/java/io/aharoj/jobapplication/review/ReviewRepository.java
@@ -0,0 +1,7 @@
+package io.aharoj.jobapplication.review;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+}
