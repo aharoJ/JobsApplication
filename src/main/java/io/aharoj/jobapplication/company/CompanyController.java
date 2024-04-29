a01b734298ae243a24dc545a6ab98afc84ab2f14
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-28 23:27:58 -0700

    delete controller

--- src/main/java/io/aharoj/jobapplication/company/CompanyController.java
+++ src/main/java/io/aharoj/jobapplication/company/CompanyController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,6 +37,12 @@ public class CompanyController {
   public ResponseEntity<String> addCompany(@RequestBody Company company) {
     companyService.createCompany(company);
     return new ResponseEntity<>("Company added sucessfully", HttpStatus.CREATED);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<String> removeCompany(@PathVariable Long id) {
+    companyService.deleteCompany(id);
+    return new ResponseEntity<>("Company deleted sucessfully", HttpStatus.OK);
   }
 
 }
