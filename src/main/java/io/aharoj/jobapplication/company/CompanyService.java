1aff049ac30b04fb6f53fe4b73a3b7f85c615b99
Angel J. Haro <haro.j.angel@gmail.com>
2024-04-28 23:26:41 -0700

    delete interface

--- src/main/java/io/aharoj/jobapplication/company/CompanyService.java
+++ src/main/java/io/aharoj/jobapplication/company/CompanyService.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 public interface CompanyService {
   List<Company> getAllCompanies();
+
   boolean updateCompany(Long id, Company company);
+
   void createCompany(Company company);
+
+  void deleteCompany(Long id);
 
 
 }
