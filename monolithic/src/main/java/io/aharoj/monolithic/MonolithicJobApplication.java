dc292db485c3bd19a9617699e7cf50f73d1fe602
Angel J. Haro <haro.j.angel@gmail.com>
2024-05-05 21:02:31 -0700

    Driver: Monolithic Application

---
+++ monolithic/src/main/java/io/aharoj/monolithic/MonolithicJobApplication.java
@@ -0,0 +1,13 @@
+package io.aharoj.monolithic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolithicJobApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MonolithicJobApplication.class, args);
+  }
+
+}
