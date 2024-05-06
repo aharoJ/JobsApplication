89023e935440edcedb5d442c535dafa5805b89bc
Angel J. Haro <haro.j.angel@gmail.com>
2024-05-05 21:39:44 -0700

    init

---
+++ microservices/company/src/test/java/io/aharoj/companyms/CompanymsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.aharoj.companyms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanymsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
